9ee12d7298154017631c32098744acca818ffd31
EC2 Default User <ec2-user@ip-172-31-9-204.ap-south-2.compute.internal>
2026-09-04 10:39:10 +0000

    Add Terraform practice configuration

---
+++ local_file.tf
@@ -0,0 +1,9 @@
+resource local_file first_file {
+
+     filename="first file"
+     
+     content="Hey! This is your first file."
+
+     file_permission= 700
+
+}
---
+++ s3.tf
@@ -0,0 +1,6 @@
+resource "aws_s3_bucket" "my_first_bucket" {
+       
+        bucket= "my-terraform-bucket-8563"
+
+
+}
